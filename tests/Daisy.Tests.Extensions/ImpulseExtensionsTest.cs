bd78e7e13268ec96a3a585dc7e788aae42607c8b
Rui Vaz <dig.int.sys@gmail.com>
2025-09-14 21:11:28 +0200

    feat: allow selecting impulse field for chains

--- tests/Daisy.Tests.Extensions/ImpulseExtensionsTest.cs
+++ tests/Daisy.Tests.Extensions/ImpulseExtensionsTest.cs
@@ -13,14 +13,33 @@ namespace Daisy.Tests.Extensions
             var impulse = new Impulse();
             impulse.AddChain("cityName: London");
             impulse.Input.Should().Be("<cityName: London>");
+            impulse.Output.Should().BeEmpty();
         }
 
         [TestMethod]
-        public void GetChainByKey_ReturnsValue()
+        public void AddChain_AppendsChainToOutput()
         {
             var impulse = new Impulse();
-            impulse.AddChain("cityName: Paris");
-            var city = impulse.Input.GetChainByKey("cityName");
+            impulse.AddChain("cityName: London", ImpulseExtensions.ImpulseField.Output);
+            impulse.Output.Should().Be("<cityName: London>");
+            impulse.Input.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetChainByKey_FromInput_ReturnsValue()
+        {
+            var impulse = new Impulse();
+            impulse.AddChain("cityName: Paris", ImpulseExtensions.ImpulseField.Input);
+            var city = impulse.GetChainByKey("cityName", ImpulseExtensions.ImpulseField.Input);
+            city.Should().Be("Paris");
+        }
+
+        [TestMethod]
+        public void GetChainByKey_FromOutput_ReturnsValue()
+        {
+            var impulse = new Impulse();
+            impulse.AddChain("cityName: Paris", ImpulseExtensions.ImpulseField.Output);
+            var city = impulse.GetChainByKey("cityName", ImpulseExtensions.ImpulseField.Output);
             city.Should().Be("Paris");
         }
     }
